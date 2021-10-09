6b51739e83cede5cc44c62e5d925f39347ea7d03
Dani <daniamir2001@yahoo.com>
2021-10-09 18:29:20 -0500

    Finished trophy script, added invisible rails

--- project/Area.gd
+++ project/Area.gd
@@ -4,12 +4,16 @@ extends Area
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var musicPlayer = get_node("Collision")
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+#func _ready():
+#	connect("body_entered" , self, "_area_entered") 
 
+#func _area_entered(object):   
+#	print("Collision detected Area")
+#	musicPlayer.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- project/Trophy.gd
+++ project/Trophy.gd
@@ -4,16 +4,21 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var musicPlayer = get_node("VictorySoundPlayer")
+onready var areaNode = get_node("Area")
+var start = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	connect("body_entered" , self, "_area_entered") 
-	connect("body_exited" , self, "_area_exited")   
+	areaNode.connect("body_entered" , self, "_area_entered") 
+	areaNode.connect("body_exited", self, "_area_exited")
 
 func _area_entered(object):   
 	print("Collision detected")
-
+	if(start == 1):
+		musicPlayer.play()
+	start += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
